5bc5800fda2e0442983a2777c60380857663669b
Alexpra2 <alexpra2@gmail.com>
2025-02-06 12:40:11 +0100

    actualizacion

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-
+//Esquema con validaciones
 const PostSchema = new mongoose.Schema({
     title: {type: String, required: [true,'el titulo es obligatorio'], minlength:[5, 'Debe tener minimo cinco caracteres'] },
     body: {type: String, required: [true,'el body es obligatorio']}
